1fdcccb11585ff165eb76379ae9826a99dbdba1d
Joshua Tapia <JOSHUATAPIA777@gmail.com>
2025-06-25 20:29:34 -0700

    removed warnings

--- sighthound/next.config.ts
+++ sighthound/next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  reactStrictMode: true, // or any other existing settings
 
-export default nextConfig;
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
+
+export default nextConfig
